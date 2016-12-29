844905ce8173acffee6dba32777b3626e5eac661
Catherine Capellen <s6cacape@uni-bonn.de>
2016-12-29 18:28:58 +0100

    loads of new questions

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # uni
 material for university
 
-quiz contains a python script to display a quiz in the terminal
-the other file contains a quiz with questions to a specific lecture.
+The folder quiz contains a python script to display a quiz in the terminal
+the other file contain quizzes for various lectures. Each file is named after the subject and the number of the lesson.
+
 The files need to have the form:
 Q: followed by the question
-A: in the following line (not more than one) with the answer
+A: with the answer
+both can only be one line long 
+
+The folder zsm contains summaries or overviews. At the moment only for the lecture Discrete and Computational Geometry
+
--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,3 +1,7 @@
+"""
+This skript displays a quiz in the format "Q:" and "A:" in alternating lines.
+They are followed by the question and the answer. Each can only be one line
+"""
 import numpy as np
 import random
 
@@ -5,47 +9,54 @@ import random
 # picture support
 # multiple line answers 
 
-filename = str(raw_input('Please enter the filename without spaces or " :'))
-f = file(filename, "r")
-questions = []
-answers = []
+def quiz():
+    filename = str(raw_input('Please enter the filename without spaces or " :'))
+    f = file(filename, "r")
+    questions = []
+    answers = []
 
-for line in f:
-    if line[0]=='Q' or line[0] == 'q':
-        questions.append(line)
-    else:
-        answers.append(line)
-        
-if len(questions) != len(answers):
-    print('there are not the same number of questions as answers')
+    for line in f:
+        if line[0]=='Q' or line[0] == 'q':
+            questions.append(line)
+        elif line[0]=='A' or line[0] == 'a':
+            answers.append(line)
+        else:
+            print 'Your quiz has a line which does not start with a "Q" or "A".'
+            
+    if len(questions) != len(answers):
+        print('There are not the same number of questions as answers.')
+    
+    studyplan = range(len(questions))
+    
+    response = str(raw_input("\nShould the questions be in random order?")) 
+    if response[0] == "Y" or response[0]=="y":
+        random.shuffle(studyplan)
+    newstudyplan = []
+    
+    print "\nWelcome to the quiz. Press q or Q to quit while in the quiz. N or n to answer no, if you didn't get the question right and it will be asked again later. \n\n"
 
-studyplan = range(len(questions))
-random.shuffle(studyplan)
-newstudyplan = []
+    while(len(studyplan)>0):
+        for q in studyplan:
+            print(questions[q])
+            response = raw_input("Do you want to see the answer?")
+            if len(response)>0:       
+                if response[0]=='Q' or response[0]=='q':
+                    studyplan = []
+                    newstudyplan = []
+                    break
+            print("\n" + str(answers[q]))
+            response = raw_input("did you get it right?: ")
+        if len(response)>0:
+            if response[0]=="N" or response[0] == 'n':
+                newstudyplan.append(q) 
+            print("\n\n")
+        if len(newstudyplan)>1:
+            studyplan = random.shuffle(newstudyplan)
+            newstudyplan = []
+        else:
+            studyplan = newstudyplan
+            newstudyplan = []
 
-print "\nWelcome to the quiz. Press q or Q to quit while in the quiz. N or n to answer no, if you didn't get the question right and it will be asked again later. \n\n"
+    print "You are done!!!"
 
-while(len(studyplan)>0):
-    for q in studyplan:
-        print(questions[q])
-        response = raw_input("Do you want to see the answer?")
-	if len(response)>0:       
-	    if response[0]=='Q' or response[0]=='q':
-	        studyplan = []
-	        newstudyplan = []
-	        break
-        print("\n" + str(answers[q]))
-        response = raw_input("did you get it right?: ")
-	if len(response)>0:
-		if response[0]=="N" or response[0] == 'n':
-		    newstudyplan.append(q) 
-        print("\n\n")
-    if len(newstudyplan)>1:
-        studyplan = random.shuffle(newstudyplan)
-        newstudyplan = []
-    else:
-        studyplan = newstudyplan
-        newstudyplan = []
-
-print "You are done!!!"
-print "Juhuu"
+quiz()
